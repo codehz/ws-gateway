61379d00dde17a7f04e734e11312883a566d5269
codehz <codehz@outlook.com>
2020-04-24 22:41:29 +0800

    fix version

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 // export * from "https://deno.hertz.services/codehz/msgpack-deno";
-export * as ws from "https://deno.land/std/ws/mod.ts";
-export * as http from "https://deno.land/std/http/mod.ts";
-export * as log from "https://deno.land/std/log/mod.ts";
-export * as logger from "https://deno.land/std/log/logger.ts";
-export * as flags from "https://deno.land/std/flags/mod.ts";
-export * as fmt from "https://deno.land/std/fmt/mod.ts";
-export * as async from "https://deno.land/std/util/async.ts";
+export * as ws from "https://deno.land/std@v0.41.0/ws/mod.ts";
+export * as http from "https://deno.land/std@v0.41.0/http/mod.ts";
+export * as log from "https://deno.land/std@v0.41.0/log/mod.ts";
+export * as logger from "https://deno.land/std@v0.41.0/log/logger.ts";
+export * as flags from "https://deno.land/std@v0.41.0/flags/mod.ts";
+export * as fmt from "https://deno.land/std@v0.41.0/fmt/mod.ts";
+export * as async from "https://deno.land/std@v0.41.0/util/async.ts";
